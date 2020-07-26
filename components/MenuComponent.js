895be350cc44248436a719c62349155f3af1fbf4
bhavesh <bkpassi2017@gmail.com>
2020-07-26 14:30:44 +0530

    Navigation Part 1

--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -1,30 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, FlatList } from 'react-native';
 import { ListItem } from 'react-native-elements';
+import { DISHES } from '../shared/dishes';
 
-function Menu(props) {
+class Menu extends Component {
 
-    const renderMenuItem = ({item, index}) => {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: DISHES
+        };
+    }
+    
+    render(){
 
-        return (
-                <ListItem
-                    key={index}
-                    title={item.name}
-                    subtitle={item.description}
-                    hideChevron={true}
-                    onPress={() => props.onPress(item.id)}
-                    leftAvatar={{ source: require('./images/uthappizza.png')}}
-                  />
-        );
-    };
+        const renderMenuItem = ({item, index}) => {
 
+            return (
+                    <ListItem
+                        key={index}
+                        title={item.name}
+                        subtitle={item.description}
+                        hideChevron={true}
+                        onPress={() => this.props.navigation.navigate("Dishdetail" ,{dishId: item.id})}
+                        leftAvatar={{ source: require('./images/uthappizza.png')}}
+                      />
+            );
+        };
+    
     return (
             <FlatList 
-                data={props.dishes}
+                data={this.state.dishes}
                 renderItem={renderMenuItem}
                 keyExtractor={item => item.id.toString()}
                 />
-    );
+    );}
 }
 
 
