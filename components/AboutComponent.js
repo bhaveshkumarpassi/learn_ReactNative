238b300cd802b2b2d7563f2fb04b89746ad70818
bhavesh <bkpassi2017@gmail.com>
2020-07-29 20:18:14 +0530

    Activity Indicators

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -3,6 +3,7 @@ import { ScrollView, View, Text, FlatList, SafeAreaView} from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import { Loading } from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -44,18 +45,43 @@ class About extends Component {
             );
         };
 
-        return (
+        if (this.props.leaders.isLoading) {
+            return(
                 <ScrollView>
-                <History />
-                <Card title={"Corporate Leadership"} >
+                    <History />
+                    <Card
+                        title='Corporate Leadership'>
+                        <Loading />
+                    </Card>
+                </ScrollView>
+            );
+        }
+        else if (this.props.leaders.errMess) {
+            return(
+                <ScrollView>
+                    <History />
+                    <Card
+                        title='Corporate Leadership'>
+                        <Text>{this.props.leaders.errMess}</Text>
+                    </Card>
+                </ScrollView>
+            );
+        }
+        else {
+            return(
+                <ScrollView>
+                    <History />
+                    <Card
+                        title='Corporate Leadership'>
                     <FlatList 
                         data={this.props.leaders.leaders}
                         renderItem={renderLeaderItem}
                         keyExtractor={item => item.id.toString()}
-                    />
-                </Card>
+                        />
+                    </Card>
                 </ScrollView>
-        );
+            );
+        }
     }
 }
 
--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -1,1 +1,3 @@
 export const baseUrl ='http://192.168.43.185:8000/';
+
+// to run json-server : npx json-server --host 0.0.0.0 --port 8000 ./db.json --watch
